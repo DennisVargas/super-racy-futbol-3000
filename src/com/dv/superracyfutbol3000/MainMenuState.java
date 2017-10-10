9d924f0305858cd6dd991dd57b77fdaf3d543de0
Dennis_Vargas <dennis.vargas@wsu.edu>
2017-10-09 20:56:16 -0700

    Updates the background of main menu to have a car and soccer ball
    Adds functionality to the New Game Text. When it is clicked the Menu selection options are switcheed to the game modes selection.

--- src/com/dv/superracyfutbol3000/MainMenuState.java
+++ src/com/dv/superracyfutbol3000/MainMenuState.java
@@ -7,7 +7,6 @@ import org.newdawn.slick.state.StateBasedGame;
 public class MainMenuState extends BasicGameState {
     int stateID = -1;
 
-
     int w = SuperRacyFutbol3000.WIDTH;
     int h = SuperRacyFutbol3000.HEIGHT;
     float s = SuperRacyFutbol3000.SCALE;
@@ -15,6 +14,8 @@ public class MainMenuState extends BasicGameState {
     int new_game_y = 250;
     int menu_item_spacing = 50;
 
+    public enum MENU_VIEW {Main, NewGame, Options, Quit}
+    MENU_VIEW current_menu_view = MENU_VIEW.Main;
     boolean on_new_game = false;
     Image [] new_game = new Image[2];
     boolean on_options = false;
@@ -22,6 +23,12 @@ public class MainMenuState extends BasicGameState {
     boolean on_quit = false;
     Image [] quit = new Image[2];
     Image background = null;
+    boolean on_1v1 = false;
+    Image [] one_v_one = new Image[2];
+    boolean on_2v2 = false;
+    Image [] two_v_two = new Image[2];
+    boolean on_3v3 = false;
+    Image [] three_v_three = new Image[2];
     //Image select_hl_img = null;
 
     public MainMenuState(int stateID){
@@ -42,6 +49,12 @@ public class MainMenuState extends BasicGameState {
         quit[0] = new Image("gfx/main_menu/quit1.png");
         quit[1] = new Image("gfx/main_menu/quit2.png");
         background = new Image("gfx/main_menu/MainMenu.png");
+        one_v_one[0] = new Image("gfx/main_menu/1v1_1.png");
+        one_v_one[1] = new Image("gfx/main_menu/1v1_2.png");
+        two_v_two[0] = new Image("gfx/main_menu/2v2_1.png");
+        two_v_two[1] = new Image("gfx/main_menu/2v2_2.png");
+        three_v_three[0] = new Image("gfx/main_menu/3v3_1.png");
+        three_v_three[1] = new Image("gfx/main_menu/3v3_2.png");
     }
 
     @Override
@@ -51,22 +64,32 @@ public class MainMenuState extends BasicGameState {
         g.fillRect(0,0,w*s,h*s);
 
         background.draw(0,0,s);
-        if (on_new_game)
-            new_game[0].draw((w*s)/8,new_game_y,s);
-        else
-            new_game[1].draw((w*s)/8,new_game_y,s);
-        if (on_options)
-            options[0].draw((w*s)/8,
-                    new_game[0].getHeight()+ new_game_y+ menu_item_spacing,s);
-        else
-            options[1].draw((w*s)/8,
-                    new_game[0].getHeight()+ new_game_y+ menu_item_spacing,s);
-        if (on_quit)
-            quit[0].draw((w*s)/8,
-                     new_game_y+ 2*(new_game[0].getHeight()+menu_item_spacing),s);
-        else
-            quit[1].draw((w*s)/8,
-                     new_game_y+ 2*(new_game[0].getHeight()+ menu_item_spacing),s);
+        switch(current_menu_view){
+            case Main:
+                if (on_new_game)
+                    new_game[0].draw((w*s)/8,new_game_y,s);
+                else
+                    new_game[1].draw((w*s)/8,new_game_y,s);
+                if (on_options)
+                    options[0].draw((w*s)/8,
+                            new_game[0].getHeight()+ new_game_y+ menu_item_spacing,s);
+                else
+                    options[1].draw((w*s)/8,
+                            new_game[0].getHeight()+ new_game_y+ menu_item_spacing,s);
+                if (on_quit)
+                    quit[0].draw((w*s)/8,
+                            new_game_y+ 2*(new_game[0].getHeight()+menu_item_spacing),s);
+                else
+                    quit[1].draw((w*s)/8,
+                            new_game_y+ 2*(new_game[0].getHeight()+ menu_item_spacing),s);
+                break;
+            case NewGame:
+                if (on_1v1)
+                    one_v_one[1].draw((w*s)/8, new_game_y,s);
+                else
+                    one_v_one[0].draw((w*s)/8, new_game_y, s);
+        }
+
     }
 
     @Override
@@ -84,6 +107,9 @@ public class MainMenuState extends BasicGameState {
             if (mouseY >= new_game_y && mouseY <= new_game_y+new_game[0].getHeight()){
                 System.out.println("y in new game");
                 on_new_game = true;
+                if (input.isMouseButtonDown(0)){
+                    current_menu_view = MENU_VIEW.NewGame;
+                }
             }
             else
                 on_new_game = false;
