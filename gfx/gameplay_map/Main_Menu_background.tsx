1c3090dc45aa1883b841d7330f6210c1df186ace
DennisVargas <dennis.vargas@wsu.edu>
2017-10-14 17:55:25 -0700

    Completes Main Menu tiled Map
    Creates a map for the main menu in tiled. Adds a new font for menu items called Trueno.

---
+++ gfx/gameplay_map/Main_Menu_background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="MainMenu" tilewidth="64" tileheight="64" tilecount="220" columns="20">
+ <image source="../main_menu/MainMenu.png" width="1280" height="720"/>
+</tileset>
