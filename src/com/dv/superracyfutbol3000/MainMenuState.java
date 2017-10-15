00b2648d7a4449987a7edeabfc28daa8f84ead42
DennisVargas <dennis.vargas@wsu.edu>
2017-10-15 15:51:47 -0700

    Removes Tiled functions form Main Menu Class
    Removes the loading of Tiled maps to render the main menu. This method was not productive. Implements new graphics for the main menu using the default slick2d methodologies to produce a Main menu Image. Removes the polling for mouse location to highlight menu selections. Defines a function called isMouseHover that will return true when this is the case.

--- src/com/dv/superracyfutbol3000/MainMenuState.java
+++ src/com/dv/superracyfutbol3000/MainMenuState.java
@@ -1,11 +1,14 @@
 package com.dv.superracyfutbol3000;
 
 import com.sun.corba.se.impl.orbutil.ObjectWriter;
+import org.mapeditor.core.ObjectGroup;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.tiled.Layer;
 import org.newdawn.slick.tiled.TiledMap;
+import org.omg.CORBA.Object;
+import org.newdawn.slick.XMLPackedSheet.*;
 
 import java.util.ArrayList;
 
@@ -16,9 +19,10 @@ public class MainMenuState extends BasicGameState {
     int h = SuperRacyFutbol3000.HEIGHT;
     float s = SuperRacyFutbol3000.SCALE;
 
-    int new_game_y = 250;
-    int menu_item_spacing = 50;
-
+    int options_y = h/2;    // options selection is the 2nd so
+                            // this should be centered and new game and quit offset from there.
+    int menu_item_spacing = 128;
+    float left_justified_x = (w*s)/8;
     public enum MENU_VIEW {Main, NewGame, Options, Quit}
     MENU_VIEW current_menu_view = MENU_VIEW.Main;
     boolean on_new_game = false;
@@ -28,16 +32,6 @@ public class MainMenuState extends BasicGameState {
     boolean on_quit = false;
     Image [] quit = new Image[2];
     Image background = null;
-    boolean on_1v1 = false;
-    Image [] one_v_one = new Image[2];
-    boolean on_2v2 = false;
-    Image [] two_v_two = new Image[2];
-    boolean on_3v3 = false;
-    Image [] three_v_three = new Image[2];
-    //Image select_hl_img = null;
-    TiledHelper main_menu_map = null;
-    Layer [] tile_layers = null;
-    ArrayList<Object> object_groups = null;
 
     public MainMenuState(int stateID){
         this.stateID = stateID;
@@ -50,16 +44,13 @@ public class MainMenuState extends BasicGameState {
 
     @Override
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-        //  load tiled map
-        main_menu_map = new TiledHelper("gfx/maps/main_menu_map2.tmx","gfx/maps/");
-        object_groups = main_menu_map.getObjectGroups();
-//        new_game[0] = new Image("gfx/main_menu/new_game1.png");
-//        new_game[1] = new Image("gfx/main_menu/new_game2.png");
-//        options[0] = new Image("gfx/main_menu/options1.png");
-//        options[1] = new Image("gfx/main_menu/options2.png");
-//        quit[0] = new Image("gfx/main_menu/quit1.png");
-//        quit[1] = new Image("gfx/main_menu/quit2.png");
-//        background = new Image("gfx/main_menu/MainMenu.png");
+        new_game[0] = new Image("gfx/main_menu/new_game3.png");
+        new_game[1] = new Image("gfx/main_menu/new_game4.png");
+        options[0] = new Image("gfx/main_menu/options3.png");
+        options[1] = new Image("gfx/main_menu/options4.png");
+        quit[0] = new Image("gfx/main_menu/quit3.png");
+        quit[1] = new Image("gfx/main_menu/quit4.png");
+        background = new Image("gfx/main_menu/MainMenu2.png");
 //        one_v_one[0] = new Image("gfx/main_menu/1v1_1.png");
 //        one_v_one[1] = new Image("gfx/main_menu/1v1_2.png");
 //        two_v_two[0] = new Image("gfx/main_menu/2v2_1.png");
@@ -70,39 +61,34 @@ public class MainMenuState extends BasicGameState {
 
     @Override
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
-        //  render map
-        main_menu_map.render(0,0);
+        //set background to white
+        g.setColor(Color.white);
+        g.fillRect(0,0,w*s,h*s);
 
-        //  set background to white
-//        g.setColor(Color.white);
-//        g.fillRect(0,0,w*s,h*s);
-//
-//        background.draw(0,0,s);
-//        switch(current_menu_view){
-//            case Main:
-//                if (on_new_game)
-//                    new_game[0].draw((w*s)/8,new_game_y,s);
-//                else
-//                    new_game[1].draw((w*s)/8,new_game_y,s);
-//                if (on_options)
-//                    options[0].draw((w*s)/8,
-//                            new_game[0].getHeight()+ new_game_y+ menu_item_spacing,s);
-//                else
-//                    options[1].draw((w*s)/8,
-//                            new_game[0].getHeight()+ new_game_y+ menu_item_spacing,s);
-//                if (on_quit)
-//                    quit[0].draw((w*s)/8,
-//                            new_game_y+ 2*(new_game[0].getHeight()+menu_item_spacing),s);
-//                else
-//                    quit[1].draw((w*s)/8,
-//                            new_game_y+ 2*(new_game[0].getHeight()+ menu_item_spacing),s);
-//                break;
-//            case NewGame:
+        background.draw(0,0,s);
+        switch(current_menu_view){
+            case Main:
+                if (on_options)
+                    options[1].draw(left_justified_x,options_y*s,s);
+                else
+                    options[0].draw(left_justified_x, options_y*s,s)
+                            /*new_game[0].getHeight()+ new_game_y+ menu_item_spacing,s)*/;
+                if (on_new_game)
+                    new_game[1].draw(left_justified_x,(options_y-menu_item_spacing)*s,s);
+                else
+                    new_game[0].draw(left_justified_x,(options_y-menu_item_spacing)*s,s);
+                if (on_quit)
+                    quit[1].draw(left_justified_x,(options_y +menu_item_spacing)*s,s);
+                else
+                    quit[0].draw(left_justified_x,(options_y + menu_item_spacing)*s,s);
+                break;
+            case NewGame:
 //                if (on_1v1)
 //                    one_v_one[1].draw((w*s)/8, new_game_y,s);
 //                else
 //                    one_v_one[0].draw((w*s)/8, new_game_y, s);
-//        }
+                break;
+        }
 
     }
 
@@ -113,37 +99,40 @@ public class MainMenuState extends BasicGameState {
         int mouseY = input.getMouseY();
 
 
-//        float new_game_minX = (w*s)/8;
-//        float options_minX = new_game_minX ;
-//        float quit_minX = new_game_minX ;
-//
+        float new_game_minX = (w*s)/8;
+        float options_minX = new_game_minX;
+        float quit_minX = new_game_minX ;
+
+//        if isMouseHover()
 //        if(mouseX >= new_game_minX &&
 //                mouseX <=new_game_minX + new_game[0].getWidth()){
-//            if (mouseY >= new_game_y && mouseY <= new_game_y+new_game[0].getHeight()){
+//            if (mouseY >= new_game_minX && mouseY <= new_game_minX+new_game[0].getHeight()){
 //                System.out.println("y in new game");
 //                on_new_game = true;
 //                if (input.isMouseButtonDown(0)){
-//                    current_menu_view = MENU_VIEW.NewGame;
+//                    // transition to new_game menu
+////                    current_menu_view = MENU_VIEW.NewGame;
 //                }
 //            }
 //            else
 //                on_new_game = false;
 //        }else
 //            on_new_game = false;
-//        if(mouseX >= options_minX+30 &&
-//                mouseX <= options_minX-30+options[0].getWidth()){
-//            if(mouseY >= new_game_y+new_game[0].getHeight()+menu_item_spacing &&
-//                    mouseY <= new_game_y+2*new_game[0].getHeight()+menu_item_spacing){
+//        if(mouseX >= options_minX &&
+//                mouseX <= options_minX+options[0].getWidth()){
+//            if(mouseY >= new_game_minX+new_game[0].getHeight()+menu_item_spacing &&
+//                    mouseY <= new_game_minX+2*new_game[0].getHeight()+menu_item_spacing){
 //                System.out.println("y in options");
 //                on_options = true;
 //            }else
 //                on_options = false;
 //        }else
 //            on_options = false;
-//        if(mouseX >= quit_minX+60 &&
-//                mouseX <= quit_minX-60+quit[0].getWidth()){
-//            if(mouseY >= new_game_y + 2*(new_game[0].getHeight()+menu_item_spacing)&&
-//                    mouseY <= new_game_y+3*new_game[0].getHeight()+2*menu_item_spacing){
+//        if(mouseX >= quit_minX &&
+//                mouseX <= quit_minX + quit[0].getWidth()){
+//            System.out.println("x in quit");
+//            if(mouseY >= new_game_minX + 2*(new_game[0].getHeight()+menu_item_spacing)&&
+//                    mouseY <= new_game_minX+3*new_game[0].getHeight() + 2*menu_item_spacing){
 //                System.out.println("y in quit");
 //                on_quit = true;
 //                if (input.isMouseButtonDown(0))
@@ -153,5 +142,17 @@ public class MainMenuState extends BasicGameState {
 //                on_quit = false;
 //        }else
 //            on_quit = false;
+    }
+
+    public boolean isMouseHover(float minX, float minY, float maxX, float maxY, float mouseX, float mouseY){
+        if( mouseX >= minX && mouseX <= maxX){
+            if (mouseY >= minY && mouseY <= maxY){
+                System.out.println("y in hover");
+                return true;
+            }
+            else
+                return false;
+        }else
+            return false;
     }
 }
