6b37f1235172a5656e5cf4834b910b1ce2982edb
Dennis_Vargas <dennis.vargas@wsu.edu>
2017-10-18 13:02:14 -0700

    Creates data types for cars.java

--- src/com/dv/superracyfutbol3000/Cars.java
+++ src/com/dv/superracyfutbol3000/Cars.java
@@ -3,5 +3,17 @@ package com.dv.superracyfutbol3000;
 import jig.Entity;
 
 public class Cars extends Entity {
+    float health_level = 0f;
+    float boost_level = 0f;
+    float acceleration = 0f;
+    float turn_angle = 0f;
+    float top_speed = 0f;
+    float top_boost_speed = 0f;
+    float max_accel = 0f;
+    float max_boost_vel = 0f;
+    float deceleration = 0f; // simulates friction, gravity, and mass
 
+    public Cars(float x, float y) {
+        super(x, y);
+    }
 }
