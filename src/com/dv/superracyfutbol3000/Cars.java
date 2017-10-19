8e1c3dd0eea457c8e45b5ef4b76b1aeaf6892908
DennisVargas <dennis.vargas@wsu.edu>
2017-10-19 10:10:48 -0700

    Adds getters and setters for all data

--- src/com/dv/superracyfutbol3000/Cars.java
+++ src/com/dv/superracyfutbol3000/Cars.java
@@ -1,19 +1,96 @@
 package com.dv.superracyfutbol3000;
 
 import jig.Entity;
+import org.newdawn.slick.Input;
 
 public class Cars extends Entity {
-    float health_level = 0f;
-    float boost_level = 0f;
-    float acceleration = 0f;
-    float turn_angle = 0f;
-    float top_speed = 0f;
-    float top_boost_speed = 0f;
-    float max_accel = 0f;
-    float max_boost_vel = 0f;
-    float deceleration = 0f; // simulates friction, gravity, and mass
+    private float health_level = 0f;
+    private float boost_level = 0f;
+    private float acceleration = 0f;
+    private float turn_angle = 0f;
+    private float top_speed = 0f;
+    private float top_boost_speed = 0f;
+    private float max_accel = 0f;
+    private float max_boost_vel = 0f;
+    private float deceleration = 0f; // simulates friction, gravity, and mass
 
     public Cars(float x, float y) {
         super(x, y);
+    }
+
+    public float GetHealthLevel() {
+        return health_level;
+    }
+
+    public void SetHealthLevel(float health_level) {
+        this.health_level = health_level;
+    }
+
+    public float GetBoostLevel() {
+        return boost_level;
+    }
+
+    public void SetBoostLevel(float boost_level) {
+        this.boost_level = boost_level;
+    }
+
+    public float GetAcceleration() {
+        return acceleration;
+    }
+
+    public void SetAcceleration(float acceleration) {
+        this.acceleration = acceleration;
+    }
+
+    public float GetTurnAngle() {
+        return turn_angle;
+    }
+
+    public void SetTurnAngle(float turn_angle) {
+        this.turn_angle = turn_angle;
+    }
+
+    public float GetTopSpeed() {
+        return top_speed;
+    }
+
+    public void SetTopSpeed(float top_speed) {
+        this.top_speed = top_speed;
+    }
+
+    public float GetTopBoostSpeed() {
+        return top_boost_speed;
+    }
+
+    public void SetTopBoostSpeed(float top_boost_speed) {
+        this.top_boost_speed = top_boost_speed;
+    }
+
+    public float GetMaxAccel() {
+        return max_accel;
+    }
+
+    public void SetMaxAccel(float max_accel) {
+        this.max_accel = max_accel;
+    }
+
+    public float GetMaxBoost_vel() {
+        return max_boost_vel;
+    }
+
+    public void SetMaxBoostVel(float max_boost_vel) {
+        this.max_boost_vel = max_boost_vel;
+    }
+
+    public float GetDeceleration() {
+        return deceleration;
+    }
+
+    public void SetDeceleration(float deceleration) {
+        this.deceleration = deceleration;
+    }
+
+    void ProcessInput(Input i){
+        //  if i matches
     }
 }
