6f248533302c4fec5c4983e05cfe6de406ab4c8d
DennisVargas <dennis.vargas@wsu.edu>
2017-10-24 01:44:06 -0700

    Renamed variables in the function checking wall collisions
    Inside of Collides HElper the function defining the collision boundary of the ellipse had variable names that didn't match with formulas about this topic. They have been changed to help understanding with notes and textbooks.

--- src/com/dv/superracyfutbol3000/CollidesHelper.java
+++ src/com/dv/superracyfutbol3000/CollidesHelper.java
@@ -93,61 +93,88 @@ public abstract class CollidesHelper {
         //  decide in play game state which ellipse it needs to pass in.
         //  if its outside of the ellipse then test the rectangle
         if(isWallDebug)System.out.println("vector_X: "+v.getX()+" vector_y: "+v.getY());
-        float center_x, center_y, radius_x, radius_y,x_center_diff, y_center_diff, radius_x_sq, radius_y_sq;
+        float center_x, center_y, radius_b, radius_a,x_center_diff, y_center_diff, radius_b_sq, radius_a_sq;
         //  simpler formulas
         //  from: ((x-c)^2)/b^2 +((y-d)^2)/a^2 = 1
         //  q = a^2/b^2
         //  a>0 and q>0
         //  (x-c_x)^2 + q(y-c_y)^2 = a^2
+        
+//      Find the angle of an object and center of ellipse
+//  ==================================
 
-        center_x = e.getCenterX();
-        center_y = e.getCenterY();
-        radius_x = e.getRadius1();
-        radius_y = e.getRadius2();
-        radius_x_sq = radius_x*radius_x;
-        radius_y_sq = radius_y*radius_y;
-        x_center_diff = (v.getX()-center_x)*(v.getX()-center_x);
-        y_center_diff = (v.getY()-center_y)*(v.getY()-center_y);
-        float x_in_ellipse = x_center_diff / radius_x_sq;
-        float y_in_ellipse = y_center_diff / radius_y_sq;
-        float q = radius_y_sq/radius_x_sq;
 
-        //  calculate the distance to the center of the ellipse
-        float in_ellipse_calc = (x_in_ellipse + y_in_ellipse);
-        float simp_in_ellipse_calc = (x_center_diff)+q*(y_center_diff);
-        //System.out.println("Distance to center of ellipse: " + d_center);
+
+        double y_diff = v.getY() - 360;
+        double y_diff_by_a = y_diff/362;
+        double theta = Math.asin(y_diff_by_a);
+   //   ======================================
 
         //  Is v in the rectangle
         if (rect.contains(v.getX(), v.getY())) {
             if(isWallDebug)System.out.println("RRRREctangle COLLIDE");
-            return false;
-        } else {
+            return false;   // not a collision if v is in the rect
+        }
+        else {
+            float a = e.getWidth()/2;
+            float b = e.getHeight()/2;
+            boolean y_axis_bigger = false;
+            //  formulas for ellipses call the biggest dimension a
+            if(b>a){
+                a = b;
+                b = e.getWidth()/2;
+                y_axis_bigger=true;
+            }
+            center_x = e.getCenterX();//    center of ellipse x coord
+            center_y = e.getCenterY();//    center of ellipse y coord
+            radius_a = e.getRadius2();//    major radius
+            radius_b = e.getRadius1();//    minor radius
+
+
+            radius_a_sq = radius_a*radius_a;//  square major radius
+            radius_b_sq = radius_b*radius_b;//  square minor radius
+
+
+            x_center_diff = (v.getX()-center_x)*(v.getX()-center_x);//  square the difference in the x direction
+            y_center_diff = (v.getY()-center_y)*(v.getY()-center_y);//  square the difference in y direction
+
+            float x_in_ellipse = x_center_diff / radius_b_sq;// is
+            float y_in_ellipse = y_center_diff / radius_a_sq;
+            float q = radius_a_sq/radius_b_sq;
+
+            //  calculate the distance to the center of the ellipse
+            float in_ellipse_calc = (x_in_ellipse + y_in_ellipse);
+            float simp_in_ellipse_calc = (x_center_diff)+q*(y_center_diff);
+            //System.out.println("Distance to center of ellipse: " + d_center);
+
+
 //            if(sqrt(simp_in_ellipse_calc) <= sqrt(radius_y_sq)){
 //                if(isWallDebug)System.out.println("Simple ellipse Point Test: "
 //                        +simp_in_ellipse_calc + " < radius_x^2"+(radius_y_sq));
 //                return false;
 //            }
 //              if v isn't in the rectangle. Is v in ellipse.
-            if ((in_ellipse_calc <= 1.030f)) {
-                if(isWallDebug)System.out.println("ellipse Point Test: "+in_ellipse_calc);
-                if(isWallDebug)System.out.println("Simple ellipse Point Test: "
-                        +simp_in_ellipse_calc + " < radius_x^2"+radius_x_sq);
+            if ((in_ellipse_calc <= 1f)) {
+//                if(isWallDebug)System.out.println("ellipse Point Test: "+in_ellipse_calc);
+//                if(isWallDebug)System.out.println("Simple ellipse Point Test: "
+//                        +simp_in_ellipse_calc + " < radius_x^2"+radius_b_sq);
                 return false;
             }
 
             //  v isn't in the rectange or the ellipse
             //  therefore its a wall collision.
             if(isWallDebug)System.out.println("Out of Ellipse");
-            if(isWallDebug)System.out.println("xterm: "+x_in_ellipse);
-            if(isWallDebug)System.out.println("yterm: "+y_in_ellipse);
+//            if(isWallDebug)System.out.println("xterm: "+x_in_ellipse);
+//            if(isWallDebug)System.out.println("yterm: "+y_in_ellipse);
             return true;
         }
     }
-    public static void CheckWorldCollisions(Teams teams, Ball ball, Ellipse left_boundary, Ellipse right_boundary,
-                                            Rectangle center_boundary){
 
-    }
-//  runs through all the collision types
+
+
+
+
+    //  runs through all the collision types
     public static CollisionType CheckCollisions(Vector v, Ellipse ellipse, Rectangle rect) {
         //  Check collide with wall
         //  Check collide with goalie
