a2e90c88d98bd7a40f798a500d414f35c836234c
DennisVargas <dennis.vargas@wsu.edu>
2017-10-24 14:28:29 -0700

    Adds direction, location, and tranlation vectors to ball. Also speed.
    Each of these has getter an setter methods. The Translation vector is built starting with a direction vector. Translation is speed*direction. location vector is cur_loc+trans. These are used in calculating the next location of the ball and if its a collision with the wall.

--- src/com/dv/superracyfutbol3000/Ball.java
+++ src/com/dv/superracyfutbol3000/Ball.java
@@ -12,16 +12,42 @@ import org.newdawn.slick.geom.Rectangle;
 
 import java.util.ArrayList;
 
+import static java.lang.Math.abs;
+
 public class Ball extends Entity{
     private final float radius = 16f;
     private float mass = 0.5f;
     private Circle ball_sphere;
     //  Holds resulting hit vectors that will be added to in the update method
-    ArrayList<CollidesHelper.CollisionReport> collision_reports;
+    ArrayList<CollidesHelper.CollisionReport> collision_reports = new ArrayList<>();
+
+    public Vector getNext_move_direction() {
+        return next_move_direction;
+    }
+
+    public void setNext_move_direction(Vector next_move_direction) {
+        this.next_move_direction = next_move_direction;
+    }
+
+    private Vector next_move_direction = new Vector(0f, 0f);
+
+    public Vector getNext_move_location() {
+        return next_move_location;
+    }
+
+    private Vector next_move_location = new Vector(0f,0f);
+    private Vector translate_next_move = new Vector(0f,0f);
+
+    private float wall_bounce_factor = 0.003f;
+    private float speed_0 = 0.2f;
+    private float speed = speed_0;
+
+
 
     public Ball(Vector position) {
         super(position);
     }
+
 
     public Ball(float x, float y) {
         super(x, y);
