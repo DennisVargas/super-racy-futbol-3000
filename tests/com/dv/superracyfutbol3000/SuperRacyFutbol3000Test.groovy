848cbb365390fc8f5f62b0303e225046eb89ebe2
DennisVargas <dennis.vargas@wsu.edu>
2017-10-25 13:43:28 -0700

    Adds Unit Testing to the project
    The junit 4 library is added so that unit testing can be performed on each class to ensure correct data is being passed along the line. Unit test for Goalies is the first created.

---
+++ tests/com/dv/superracyfutbol3000/SuperRacyFutbol3000Test.groovy
@@ -0,0 +1,22 @@
+package com.dv.superracyfutbol3000
+
+class SuperRacyFutbol3000Test extends GroovyTestCase {
+    void testInitStatesList() {
+    }
+
+    void testMain() {
+        SuperRacyFutbol3000.main("-d")
+
+//        SuperRacyFutbol3000.play_settings.SetPlayersPerTeam(players_per_team);
+//
+//
+//        //  default 1 player on keyboard controls for bookmark
+//        //  players should activate depending on which control is being used first
+//        //  players should also be able to choose which controller they want to use
+//        human_players.add(new Players());
+//        SuperRacyFutbol3000.play_settings.players = human_players;
+//        SuperRacyFutbol3000.play_settings.SetHumanPlayers(human_players.size());
+//        SuperRacyFutbol3000.play_settings.SetTeams();
+//        stateBasedGame.enterState(SuperRacyFutbol3000.PLAYSTATE);
+    }
+}
