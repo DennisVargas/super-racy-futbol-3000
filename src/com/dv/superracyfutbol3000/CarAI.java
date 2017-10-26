de376ecb47569a751027ae709a754b002e992b78
DennisVargas <dennis.vargas@wsu.edu>
2017-10-25 20:59:17 -0700

    Creates file for Car AI class
    Starts with an enum of Needs. needBall, boost, defense, and health. Methods Where Health and Boost will locate where on teh board a new health and boost has spawned. GetMyGoal and get their goal will serve as location markers.

---
+++ src/com/dv/superracyfutbol3000/CarAI.java
@@ -0,0 +1,29 @@
+package com.dv.superracyfutbol3000;
+
+import jig.Vector;
+
+public class CarAI {
+    enum Needs {needBall, needBoost, needDefense, needHealth}
+
+    public Vector WhereHealth(){
+        //  scans board for health when it needs health
+        return null;
+    }
+
+    public Vector WhereBoost(){
+        return null;
+    }
+
+    public Vector getMyGoal(){
+        return null;
+    }
+
+    public Vector getTheirGoal (){
+        return null;
+    }
+
+
+
+
+
+}
