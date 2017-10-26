51dae65045802cd1e18daf97495adde3cc1b7d37
DennisVargas <dennis.vargas@wsu.edu>
2017-10-25 21:21:52 -0700

    Adds dimmentions to the bounding boxes for the goals
    These dimensions may be used in score board.
    Goals are more or less a trigger mechanic that uses collides to trigger a goal with the ball.

--- src/com/dv/superracyfutbol3000/Goals.java
+++ src/com/dv/superracyfutbol3000/Goals.java
@@ -1,4 +1,25 @@
 package com.dv.superracyfutbol3000;
 
-public class Goals {
+import jig.Entity;
+
+import java.awt.*;
+
+public class Goals extends Entity{
+
+    //  red goal box low right = 64,512
+    //               low left = 0,512
+    //               upper right64,192
+    //               upper left 0,192
+    //  blue goal box low right=     1280,512
+//                      low left= 1216,512
+                    //  up left =  1216,192
+    //                  up right =  1280,192
+
+    Rectangle goalRectangle;
+
+    public Goals(){
+    }
+
+
+
 }
