b77e3a593fa4c54fc1fa80b1f278fa0066a7beba
DennisVargas <dennis.vargas@wsu.edu>
2017-10-26 11:45:43 -0700

    Creates a carcarcollision tests in collideshelpertest
    This test failed as the car needs the JIG ResouurceManager to load up the image of the car. Tested inside production code :|.

--- tests/com/dv/superracyfutbol3000/CollidesHelperTest.java
+++ tests/com/dv/superracyfutbol3000/CollidesHelperTest.java
@@ -1,5 +1,36 @@
+package com.dv.superracyfutbol3000;
+
+import jig.Entity;
+import jig.Vector;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CollidesHelperTest {
+    Cars carA;
+    Cars carB;
+    @BeforeAll
+    public static void init(){
+        Entity.setCoarseGrainedCollisionBoundary(Entity.CIRCLE);
 
+    }
+
+    @BeforeEach
+    void setup(){
+
+
+    }
+
+    @Test
+    void testIsCarCarCollision() {
+        //  Does carA hit car BCollidesHelper.CarCarCollide(carA, carB);
+        //  carA next move hits carB curPosition.
+        //  At most
+
+        //  getting back an array list with the two resulting direction vectors would be useful
+        Assertions.assertNotNull(CollidesHelper.CarCarCollides(carA, carB));
+    }
 }
