ff3f98553b23e3789e7a032360aad42a86451916
DennisVargas <dennis.vargas@wsu.edu>
2017-10-26 01:22:50 -0700

    Creates tests that assert positions which aren't goals
    these all reaturn false.

--- tests/com/dv/superracyfutbol3000/GoalsTest.java
+++ tests/com/dv/superracyfutbol3000/GoalsTest.java
@@ -71,4 +71,29 @@ class GoalsTest {
         //  Returns a score of 1 if the position is a goal within that goal
         Assertions.assertEquals(1,blue_goal.IsGoal(ball_pos, radius));
     }
+
+    @Test
+    void testIsGoalScoreOnRedNotGoal() {
+        //  width plus the center point should bush the ball out
+        Vector ball_pos = new Vector(red_goal.getRectangle().getWidth()+red_goal.getRectangle().getCenterX(),
+                                        red_goal.getRectangle().getHeight()+red_goal.getRectangle().getCenterY());
+        float radius = 20f;
+        Assertions.assertEquals(0, red_goal.IsGoal(ball_pos, radius));
+    }
+    @Test
+    void testIsGoalScoreOnBlueNotGoal() {
+        Vector ball_pos = new Vector(blue_goal.getRectangle().getWidth()+blue_goal.getRectangle().getCenterX(),
+                blue_goal.getRectangle().getHeight()+blue_goal.getRectangle().getCenterY());
+        float radius = 20f;
+        Assertions.assertEquals(0, blue_goal.IsGoal(ball_pos, radius));
+    }
+
+    @Test
+    void testIsGoalScoreOnRedXEdgeGoal() {
+        Vector ball_pos = new Vector(-blue_goal.getRectangle().getWidth()/2+blue_goal.getRectangle().getCenterX(),
+                blue_goal.getRectangle().getHeight()/2+blue_goal.getRectangle().getCenterY());
+        float radius = 20f;
+        Assertions.assertEquals(0, blue_goal.IsGoal(ball_pos, radius));
+    }
+
 }
