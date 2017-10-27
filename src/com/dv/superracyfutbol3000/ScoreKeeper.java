5aa70d062dccee54b1445d61288d68eb7e695634
DennisVargas <dennis.vargas@wsu.edu>
2017-10-26 22:50:43 -0700

    Creates score keeper class
    Score keeper has two ints to hold blue score and red score. The methods to increment take an int and add it to these scores. Test for winner happens in Is BlueWinner and also for REd.

--- src/com/dv/superracyfutbol3000/ScoreKeeper.java
+++ src/com/dv/superracyfutbol3000/ScoreKeeper.java
@@ -2,10 +2,51 @@ package com.dv.superracyfutbol3000;
 import java.lang.Math;
 public class ScoreKeeper {
 
-    public ScoreKeeper() {
 
+    private int red_score;
+    private int blue_score;
+
+    public ScoreKeeper() {
+        this.blue_score = 0;
+        this.red_score = 0;
     }
-    private void func(){
+
+    public int getRedScore() {
+        return red_score;
+    }
+
+    public int getBlueScore() {
+        return blue_score;
+    }
+
+    public void setRedScore(int red_score) {
+        this.red_score = red_score;
+    }
+
+    public void setBlueScore(int blue_score) {
+        this.blue_score = blue_score;
+    }
+
+    public void IncrementRedScore(int increment) {
+        this.red_score += increment;
+    }
+
+    public void IncrementBlueScore(int increment) {
+        this.blue_score += increment;
+    }
+
+    public boolean IsBlueWinner() {
+        if (this.blue_score> this.red_score)
+            return true;
+        else
+            return false;
+    }
+
+    public boolean IsRedWinner() {
+        if(this.red_score> this.blue_score)
+            return true;
+        else
+            return false;
 
     }
 }
