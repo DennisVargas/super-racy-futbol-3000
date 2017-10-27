b5e4fce47405cb3678c79f18009699db98cc8b66
DennisVargas <dennis.vargas@wsu.edu>
2017-10-27 00:07:50 -0700

    Creates values for the position of the most and least significant digits

---
+++ src/com/dv/superracyfutbol3000/ScoreBoard.java
@@ -0,0 +1,18 @@
+package com.dv.superracyfutbol3000;
+
+import jig.Entity;
+import jig.Vector;
+
+public class ScoreBoard{
+    Vector red_most_sig_dig = new Vector(47 ,69);
+    Vector blue_most_sig_dig = new Vector(1206,69);
+    Vector red_least_sig_dig = new Vector(81,69);
+    Vector blue_least_sig_dig = new Vector(1240,69);
+
+    Entity red_scoreboard;
+    Entity blue_scoreboard;
+
+    void InitBoards(){
+        red_scoreboard = new Entity()
+    }
+}
---
+++ tests/com/dv/superracyfutbol3000/ScoreBoardTest.java
@@ -0,0 +1,9 @@
+package com.dv.superracyfutbol3000;
+
+import jig.Vector;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScoreBoardTest {
+
+}
