e19eefdf747536ec6efaa9b847cdadcdb1a35a94
DennisVargas <dennis.vargas@wsu.edu>
2017-10-29 20:29:55 -0700

    Creates a class defining MoveOrder
    MoveOrder defines the enum CarCommands which have commands for accelerate, decelerate and turning. There are two private member variables; accel_command and turn_command. Each move order must set these or they will be set to None by default.

--- src/com/dv/superracyfutbol3000/MoveOrder.java
+++ src/com/dv/superracyfutbol3000/MoveOrder.java
@@ -1,4 +1,32 @@
 package com.dv.superracyfutbol3000;
 
 public class MoveOrder {
+    enum CarCommands {none,accelerate, deccelerate, turn_left, turn_right, straight}
+    private CarCommands accel_command, turn_command;
+
+    public MoveOrder() {
+        setTurn_command(CarCommands.none);
+        setAccelCommand(CarCommands.none);
+    }
+
+    public MoveOrder(CarCommands accel_command, CarCommands turn_command) {
+        setAccelCommand(accel_command);
+        setTurn_command(turn_command);
+    }
+
+    public void setAccelCommand(CarCommands accel_command){
+        this.accel_command = accel_command;
+    }
+
+    public void setTurn_command(CarCommands turn_command){
+        this.turn_command = turn_command;
+    }
+
+    public CarCommands getAccelCommand(){
+        return this.accel_command;
+    }
+
+    public CarCommands getTurnCommand() {
+        return this.turn_command;
+    }
 }
